ee7bd3f230c2278cb9da76c6294f5047880ad9f3
VWRoli <fustroli@gmail.com>
2020-12-23 17:41:13 +0100

    added fav button functionaliy

--- Beetle Web Gallery/js/main.js
+++ Beetle Web Gallery/js/main.js
@@ -1,7 +1,8 @@
 'use strict';
-
 const imgArr = [1, 2, 3];
 let counter = 1;
+const favEl = document.querySelector('.far');
+const favElAll = document.querySelectorAll('.far');
 
 //Change BG every 10 seconds
 setInterval(function () {
@@ -12,3 +13,15 @@ setInterval(function () {
     '#showcase'
   ).style.background = `url(../images/showcase-bg-${counter}.jpg) no-repeat center/cover`;
 }, 10000);
+
+//Favorite Button
+for (let i = 0; i < favElAll.length; i++) {
+  favElAll[i].addEventListener('click', function () {
+    this.classList.toggle('fav-scale');
+    if (this.style.color === '') {
+      this.style.color = `var(--primary-btn-color)`;
+    } else {
+      this.style.color = ``;
+    }
+  });
+}
