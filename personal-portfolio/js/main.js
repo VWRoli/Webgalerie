16a181e1d30f2609d0ade225cb153d5a8e41a894
VWRoli <fustroli@gmail.com>
2021-01-03 11:44:09 +0100

    completed navbar

--- personal-portfolio/js/main.js
+++ personal-portfolio/js/main.js
@@ -1,1 +1,5 @@
 'use strict';
+//Nav burger Toggle Button
+document.querySelector('#burger-icon').addEventListener('click', function () {
+  document.querySelector('.toggle').classList.toggle('active');
+});
