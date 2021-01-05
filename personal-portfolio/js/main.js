2e20de4c68ef763c80f1aba2127de258c2434ea3
VWRoli <fustroli@gmail.com>
2021-01-05 11:24:51 +0100

    added full responsivity

--- personal-portfolio/js/main.js
+++ personal-portfolio/js/main.js
@@ -1,5 +1,13 @@
 'use strict';
+//Variables
+const menuElAll = document.querySelectorAll('.menu-items');
 //Nav burger Toggle Button
 document.querySelector('#burger-icon').addEventListener('click', function () {
   document.querySelector('.toggle').classList.toggle('active');
 });
+//Close drop down menu when an item clicked
+for (let i = 0; i < menuElAll.length; i++) {
+  menuElAll[i].addEventListener('click', function () {
+    document.querySelector('.toggle').classList.toggle('active');
+  });
+}
