7b409e8a5f5bc10faecce7a684fa16795df3be19
VWRoli <fustroli@gmail.com>
2021-01-11 16:25:03 +0100

    working login buttons

--- Beetle Web Gallery/js/main.js
+++ Beetle Web Gallery/js/main.js
@@ -1,7 +1,17 @@
 'use strict';
+//Variables
 let counter = 1;
 const favEl = document.querySelector('.far');
 const favElAll = document.querySelectorAll('.far');
+//login, signup
+const btnLoginEL = document.querySelector('.btn-login');
+const btnSignupEL = document.querySelector('.btn-signup');
+const signupDisplay = document.querySelector('.signup-display');
+const loginDisplay = document.querySelector('.login-display');
+const signupOverlay = document.querySelector('.signup-overlay');
+const loginContainer = document.querySelector('.login-container');
+const innerLogin = document.querySelector('#inner-login-form');
+const innerSignup = document.querySelector('#inner-signup-form');
 
 //Change BG every 10 seconds
 setInterval(function () {
@@ -22,3 +32,56 @@ for (let i = 0; i < favElAll.length; i++) {
     }
   });
 }
+//////////////////////////////////////////////////
+//Login and Signup Button
+
+loginContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const login = e.target.classList.contains(`btn-login`);
+  if (login) {
+    //Open login form
+    loginDisplay.classList.remove('hidden');
+    signupOverlay.classList.remove('hidden');
+  } else {
+    //Open signup form
+    signupDisplay.classList.remove('hidden');
+    signupOverlay.classList.remove('hidden');
+  }
+});
+signupOverlay.addEventListener('click', function () {
+  signupOverlay.classList.add('hidden');
+  loginDisplay.classList.add('hidden');
+  signupDisplay.classList.add('hidden');
+});
+//
+const switchforms = () => {
+  signupDisplay.classList.toggle('hidden');
+  loginDisplay.classList.toggle('hidden');
+};
+innerLogin.addEventListener('click', function (e) {
+  //e.preventDefault();
+  switchforms();
+});
+
+innerSignup.addEventListener('click', function (e) {
+  //e.preventDefault();
+  switchforms();
+});
+///////////////////////////////////////////////////
+//Accounts
+const account1 = {
+  nickname: 'roland',
+  password: '1234',
+};
+const account2 = {
+  nickname: 'john',
+  password: '4321',
+};
+const account3 = {
+  nickname: 'sarah',
+  password: '9876',
+};
+const account4 = {
+  nickname: 'smith',
+  password: '6789',
+};
