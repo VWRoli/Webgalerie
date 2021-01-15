8146b1adf254806a5a07ca0a2d67943891884c6b
VWRoli <fustroli@gmail.com>
2021-01-15 15:45:17 +0100

    added set theme function with localstorage

--- Glass-portfolio-design/js/main.js
+++ Glass-portfolio-design/js/main.js
@@ -9,6 +9,34 @@ const mainWrapper = document.querySelector('.main-wrapper');
 const projectsWrapper = document.querySelector('.projects');
 const detailsWrapper = document.querySelector('.details-wrapper');
 
+const menuElAll = document.querySelector('.menu-nav');
+const menuBtn = document.querySelectorAll('.menu-btn');
+
+const themeBtns = document.querySelector('#theme-options-wrapper');
+const themeStyle = document.querySelector('#theme-style');
+
+//Change theme
+//get from localstorage
+let theme = localStorage.getItem('theme');
+
+themeBtns.addEventListener('click', function (e) {
+  if (e.target.classList.contains('theme-btn')) {
+    const mode = e.target.dataset.mode;
+    setTheme(mode);
+  }
+});
+
+const setTheme = (mode) => {
+  if (mode === 'winter') themeStyle.href = 'css/style.css';
+  if (mode === 'spring') themeStyle.href = 'css/spring.css';
+  if (mode === 'summer') themeStyle.href = 'css/summer.css';
+  if (mode === 'autumn') themeStyle.href = 'css/autumn.css';
+  localStorage.setItem('theme', mode);
+};
+//save to localStorage
+theme === null ? setTheme('winter') : setTheme(theme);
+
+//content slider functions
 const slide = (percentage) => {
   mainWrapper.style.transform = `translateY(${percentage}%)`;
   projectsWrapper.style.transform = `translateY(${percentage}%)`;
@@ -27,9 +55,6 @@ const slider = (target) => {
 
 //menu-nav
 mainPageBtn.classList.add('active-menu');
-
-const menuElAll = document.querySelector('.menu-nav');
-const menuBtn = document.querySelectorAll('.menu-btn');
 
 menuElAll.addEventListener('click', function (e) {
   const clicked = e.target.closest('li');
