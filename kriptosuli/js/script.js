a3ee7e7f8bd7d269fb66032287dc07e219879595
VWRoli <fustroli@gmail.com>
2021-01-17 17:52:13 +0100

    added active buttons function

--- kriptosuli/js/script.js
+++ kriptosuli/js/script.js
@@ -5,6 +5,8 @@ const navbarEl = document.querySelector('.navbar');
 
 const themeBtns = document.querySelector('.theme-btns');
 const themeStyle = document.querySelector('#theme-style');
+const darkBtn = document.querySelector('.fa-moon');
+const lightBtn = document.querySelector('.fa-sun');
 
 //Toggle navbar
 burgerIcon.addEventListener('click', function () {
@@ -19,13 +21,20 @@ themeBtns.addEventListener('click', function (e) {
   if (e.target.classList.contains('theme-btn')) {
     const mode = e.target.dataset.mode;
     setTheme(mode);
-    console.log(e.target.dataset.mode);
   }
 });
 
 const setTheme = (mode) => {
-  if (mode === 'light') themeStyle.href = 'css/style.css';
-  if (mode === 'dark') themeStyle.href = 'css/dark.css';
+  if (mode === 'light') {
+    themeStyle.href = 'css/style.css';
+    darkBtn.classList.remove('theme-active');
+    lightBtn.classList.add('theme-active');
+  }
+  if (mode === 'dark') {
+    themeStyle.href = 'css/dark.css';
+    lightBtn.classList.remove('theme-active');
+    darkBtn.classList.add('theme-active');
+  }
   localStorage.setItem('theme', mode);
 };
 //save to localStorage
