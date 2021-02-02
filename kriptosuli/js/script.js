bb87fa52cd774b0e075e6fdb023a7f714854edc7
VWRoli <fustroli@gmail.com>
2021-02-02 11:30:14 +0100

    added market prices function

--- kriptosuli/js/script.js
+++ kriptosuli/js/script.js
@@ -31,3 +31,39 @@ setTheme();
 themeBtn.addEventListener('change', function () {
   setTheme();
 });
+
+///////////////////////////////////////////////////////////
+//CoinGecko API
+const tableBody = document.querySelector('.table-body');
+
+const renderCoinData = function (coins) {
+  console.log(coins);
+
+  coins.forEach((coin) => {
+    const html = `
+    <tr class="tr-grid">
+     <td class="coin-rank">${coin.market_cap_rank}</td>
+     <td class="coin-icon"><img src="${coin.image}" alt="" /></td>
+     <td class="coin-symbol">${coin.symbol.toUpperCase()}</td>
+     <td class="coin-name">${coin.name}</td>
+     <td class="coin-price">${coin.current_price}</td>
+     <td class="coin-price-change">${coin.price_change_percentage_24h}</td>
+     <td class="coin-supply">${coin.circulating_supply.toFixed(
+       0
+     )} ${coin.symbol.toUpperCase()}</td>
+    </tr>
+    `;
+    tableBody.insertAdjacentHTML('beforeend', html);
+  });
+};
+
+const getCoinMarkets = async function () {
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+  );
+  //if (!res.ok) throw new Error('Problem getting coin data');
+
+  const data = await res.json();
+  renderCoinData(data);
+};
+getCoinMarkets();
