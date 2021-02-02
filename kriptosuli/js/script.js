dbc6ec1b21ab623a2fbdb81d28dcc4f35b7d6a26
VWRoli <fustroli@gmail.com>
2021-02-02 15:10:35 +0100

    market price table formatting

--- kriptosuli/js/script.js
+++ kriptosuli/js/script.js
@@ -6,6 +6,9 @@ const navbarEl = document.querySelector('.navbar');
 const themeStyle = document.querySelector('#theme-style');
 const themeBtn = document.querySelector('.theme-btn');
 
+const tableBody = document.querySelector('.table-body');
+const coinPriceChangeLabel = document.querySelector('.coin-price-change');
+
 //Toggle navbar
 burgerIcon.addEventListener('click', function () {
   navbarEl.classList.toggle('active-nav');
@@ -34,32 +37,70 @@ themeBtn.addEventListener('change', function () {
 
 ///////////////////////////////////////////////////////////
 //CoinGecko API
-const tableBody = document.querySelector('.table-body');
 
 const renderCoinData = function (coins) {
   console.log(coins);
 
   coins.forEach((coin) => {
+    //Format price
+    const price = priceFormatter(coin.current_price);
+
+    //Format price change
+    const priceChange = priceChangeFormatter(coin.price_change_percentage_24h);
+    //Price change color
+    const type = coin.price_change_percentage_24h > 0 ? `positive` : `negative`;
+
+    //Format supply
+    const supply = supplyFormatter(coin.circulating_supply);
+
+    //Display table
     const html = `
     <tr class="tr-grid">
      <td class="coin-rank">${coin.market_cap_rank}</td>
      <td class="coin-icon"><img src="${coin.image}" alt="" /></td>
      <td class="coin-symbol">${coin.symbol.toUpperCase()}</td>
      <td class="coin-name">${coin.name}</td>
-     <td class="coin-price">${coin.current_price}</td>
-     <td class="coin-price-change">${coin.price_change_percentage_24h}</td>
-     <td class="coin-supply">${coin.circulating_supply.toFixed(
-       0
-     )} ${coin.symbol.toUpperCase()}</td>
+     <td class="coin-price">${price}</td>
+     <td class="coin-price-change ${type}">${priceChange}</td>
+     <td class="coin-supply">${supply} ${coin.symbol.toUpperCase()}</td>
     </tr>
     `;
     tableBody.insertAdjacentHTML('beforeend', html);
   });
 };
+//Formatter functions
+//Format supply
+const supplyFormatter = (supply) => {
+  const formattedSupply = new Intl.NumberFormat('hu-HU', {
+    notation: 'compact',
+    compactDisplay: 'long',
+  }).format(supply);
+  return formattedSupply;
+};
+//Format Price Change
+const priceChangeFormatter = (priceChange) => {
+  const formattedPriceChange = new Intl.NumberFormat('hu-HU', {
+    style: 'percent',
+    signDisplay: 'exceptZero',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(priceChange / 100);
 
+  return formattedPriceChange;
+};
+//Format price
+const priceFormatter = (price) => {
+  const formattedPrice = new Intl.NumberFormat('hu-HU', {
+    style: 'currency',
+    currency: 'HUF',
+  }).format(price);
+  return formattedPrice;
+};
+
+//
 const getCoinMarkets = async function () {
   const res = await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=huf&order=market_cap_desc&per_page=10&page=1&sparkline=false`
   );
   //if (!res.ok) throw new Error('Problem getting coin data');
 
