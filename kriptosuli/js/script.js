26e87ae1390c5e948e333eaa75f9b1e2d7faebd8
VWRoli <fustroli@gmail.com>
2021-02-02 15:29:11 +0100

    added toggle button for market-price table

--- kriptosuli/js/script.js
+++ kriptosuli/js/script.js
@@ -9,6 +9,9 @@ const themeBtn = document.querySelector('.theme-btn');
 const tableBody = document.querySelector('.table-body');
 const coinPriceChangeLabel = document.querySelector('.coin-price-change');
 
+const marketTable = document.querySelector('.market-table');
+const hideMarketBtn = document.querySelector('.hide-market-btn');
+
 //Toggle navbar
 burgerIcon.addEventListener('click', function () {
   navbarEl.classList.toggle('active-nav');
@@ -35,12 +38,26 @@ themeBtn.addEventListener('change', function () {
   setTheme();
 });
 
+//Toggle market price table
+hideMarketBtn.addEventListener('click', function () {
+  marketTable.classList.toggle('visibility');
+});
 ///////////////////////////////////////////////////////////
 //CoinGecko API
+//Get data from CoinGecko
+const getCoinMarkets = async function () {
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=huf&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+  );
+  //if (!res.ok) throw new Error('Problem getting coin data');
 
+  const data = await res.json();
+  renderCoinData(data);
+};
+getCoinMarkets();
+
+//Render data
 const renderCoinData = function (coins) {
-  console.log(coins);
-
   coins.forEach((coin) => {
     //Format price
     const price = priceFormatter(coin.current_price);
@@ -93,18 +110,8 @@ const priceFormatter = (price) => {
   const formattedPrice = new Intl.NumberFormat('hu-HU', {
     style: 'currency',
     currency: 'HUF',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
   }).format(price);
   return formattedPrice;
 };
-
-//
-const getCoinMarkets = async function () {
-  const res = await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=huf&order=market_cap_desc&per_page=10&page=1&sparkline=false`
-  );
-  //if (!res.ok) throw new Error('Problem getting coin data');
-
-  const data = await res.json();
-  renderCoinData(data);
-};
-getCoinMarkets();
