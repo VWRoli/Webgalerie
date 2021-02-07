17467f34f581c793c93a6e06cdfde2713738e3c8
VWRoli <fustroli@gmail.com>
2021-02-07 18:27:25 +0100

    added coin sorting option

--- kriptosuli/js/market.js
+++ kriptosuli/js/market.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const tableHeader = document.querySelector('.thead');
 const tableBody = document.querySelector('.table-body');
 const coinPriceChangeLabel = document.querySelector('.coin-price-change');
 
@@ -32,41 +33,31 @@ const getCoinMarkets = async function () {
 getCoinMarkets();
 
 //Render error
-const renderError = function (msg) {
+function renderError(msg) {
   tableBody.insertAdjacentText('beforeend', msg);
-};
+}
 
-//Render data
-const renderCoinData = function (coins) {
-  coins.forEach((coin) => {
-    //Format price
-    const price = priceFormatter(coin.current_price);
-
-    //Format price change
-    const priceChange = priceChangeFormatter(coin.price_change_percentage_24h);
-    //Price change color
-    const type = coin.price_change_percentage_24h > 0 ? `positive` : `negative`;
-
-    //Format supply and market cap
-    const supply = numberFormatter(coin.circulating_supply);
-    const marketcap = numberFormatter(coin.market_cap);
-
-    //Display table
-    const html = `
-      <tr class="tr-grid">
-       <td class="coin-rank">${coin.market_cap_rank}</td>
-       <td class="coin-icon"><img src="${coin.image}" alt="" /></td>
-       <td class="coin-symbol">${coin.symbol.toUpperCase()}</td>
-       <td class="coin-name">${coin.name}</td>
-       <td class="coin-price">${price}</td>
-       <td class="coin-price-change ${type}">${priceChange}</td>
-       <td class="coin-marketcap">${marketcap}</td>
-       <td class="coin-supply">${supply} ${coin.symbol.toUpperCase()}</td>
-      </tr>
-      `;
-    tableBody.insertAdjacentHTML('beforeend', html);
+//Display coins
+function renderCoinData(coins) {
+  tableHeader.addEventListener('click', function (e) {
+    const clicked = e.target;
+    let columnClicked = e.target.dataset.column;
+    let columnOrder = e.target.dataset.order;
+    //Sorting
+    if (columnOrder === 'desc') {
+      clicked.setAttribute('data-order', 'asc');
+      coins.sort((a, b) => (a[columnClicked] > b[columnClicked] ? 1 : -1));
+    } else {
+      clicked.setAttribute('data-order', 'desc');
+      coins.sort((a, b) => (b[columnClicked] < a[columnClicked] ? -1 : 1));
+    }
+    tableBody.innerHTML = '';
+    //Render sorted coins to display
+    coins.forEach(buildTableData);
   });
-};
+  //Render coins to display
+  coins.forEach(buildTableData);
+}
 //Formatter functions
 //Format supply and marketcap
 const numberFormatter = (supply) => {
@@ -96,4 +87,34 @@ const priceFormatter = (price) => {
     maximumFractionDigits: 0,
   }).format(price);
   return formattedPrice;
+};
+
+//Build table data
+const buildTableData = (coin) => {
+  //Format price
+  const price = priceFormatter(coin.current_price);
+
+  //Format price change
+  const priceChange = priceChangeFormatter(coin.price_change_percentage_24h);
+  //Price change color
+  const type = coin.price_change_percentage_24h > 0 ? `positive` : `negative`;
+
+  //Format supply and market cap
+  const supply = numberFormatter(coin.circulating_supply);
+  const marketcap = numberFormatter(coin.market_cap);
+
+  //Display table
+  const html = `
+      <tr class="tr-grid">
+       <td class="coin-rank">${coin.market_cap_rank}</td>
+       <td class="coin-icon"><img src="${coin.image}" alt="" /></td>
+       <td class="coin-symbol">${coin.symbol.toUpperCase()}</td>
+       <td class="coin-name">${coin.name}</td>
+       <td class="coin-price">${price}</td>
+       <td class="coin-price-change ${type}">${priceChange}</td>
+       <td class="coin-marketcap">${marketcap}</td>
+       <td class="coin-supply">${supply} ${coin.symbol.toUpperCase()}</td>
+      </tr>
+      `;
+  tableBody.insertAdjacentHTML('beforeend', html);
 };
